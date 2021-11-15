4ac43ab13c7b282e33e5498e9f1715917ff076b4
AshaMarj <crockera2020@gmail.com>
2021-11-14 19:29:04 -0500

    Uploaded files
    Uploaded files

---
+++ weather.js
@@ -0,0 +1,45 @@
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let day = currentTime.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "ad409a1d462fddc9d1a0d6eab41325a2";
+  let city = document.querySelector("#city-query").value;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let searchForm = document.querySelector("#search-form");
+
+searchForm.addEventListener("submit", search);
+
+dateElement.innerHTML = `${days[day]} ${hours}:${minutes}`;
